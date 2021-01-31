aeb689076730f8dd8e875d51c3ca5f4cc29fc003
skysaw144 <alexandermesko@icloud.com>
2021-01-31 17:06:41 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                Patron Technology, Puzzle Portion;
+                <input type={"file"}/>
+
+            </header>
+            <table>
+                <thead>
+                <tr>
+                    <th colSpan={2}>Table headers</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>The table body</td>
+                    <td>with two columns</td>
+                </tr>
+                <tr>
+                    <td>The table body</td>
+                    <td>with two columns</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
 export default App;
